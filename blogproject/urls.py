8ed95b9796993c6faeadb489cc9c60d7cbf921dc
hyehyeon <hyehyeon0329@gmail.com>
2019-03-07 19:05:19 +0900

    second commit

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name="home"),
-    path('blog/', include('blogapp.urls')),
+    path('blog/', include('blogapp.urls')),et
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
     path('accounts/', include('accounts.urls')),
 ]
